f63824ebd8e04ffef25d08e2ec4c124882cd6f05
a5hish <0theundefined@gmail.com>
2018-01-14 23:07:41 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,18 +2,20 @@ import os
 import shutil
 from setuptools import setup, find_packages
 
-temp_dir = os.path.expanduser("~")+"/Templates/gtree-templates"
-if not os.path.isdir(temp_dir):
-	os.makedirs(tmp_dir)
+temp_dir = os.path.expanduser("~")+"/Templates/gtree-templates/"
+if os.path.isdir(temp_dir) == False:
+	os.makedirs(temp_dir)
 	shutil.copytree("sample-templates/", temp_dir)
+	os.rename()
 
 
 setup(
 	name = "gtree",
-	version = "0.1dev",
-	description = "Gtree is an simple script which can be used to create directory-structure as defined in an text-based file.",
+	version = "0.1",
+	description = "gtree is a simple program which automatically create directory-templates form a text file",
 	author = "a5hish",
 	packages = ["gtree"],
+	license = 'GPLv3',
 	entry_points={
           'console_scripts': ['gtree = gtree.__main__:main'],
       }
